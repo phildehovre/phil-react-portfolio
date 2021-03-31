49112bccdf7c0cc9b4064813d0342692ba8bae1e
phildehvre <ph.dehovre@gmail.com>
2021-03-31 15:21:14 +0200

    multiple APIS working CHECK VARIABLES

--- src/components/projects/metronome/SongList.js
+++ src/components/projects/metronome/SongList.js
@@ -12,8 +12,8 @@ class SongList extends React.Component {
   }
   
   componentDidMount() {
-    const KEY = '93f2be180a4be75f06c1a7d2829e8bbc'
-    axios.get(`https://api.getsongbpm.com/tempo/?api_key=${KEY}&bpm=${this.props.bpm}`, {
+    const KEY = env.BPM_API_KEY
+        axios.get(`https://api.getsongbpm.com/tempo/?api_key=${KEY}&bpm=${this.props.bpm}`, {
     })
     .then(res => {
       const sampledSongs = res.data.tempo.slice(0, this.state.listSize)
