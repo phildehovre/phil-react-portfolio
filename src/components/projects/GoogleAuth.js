b5ca0d9c0f140db53c5ace37f17e774a36e1f123
phildehvre <ph.dehovre@gmail.com>
2021-04-18 09:57:38 +0100

    fixed header phone

--- src/components/projects/GoogleAuth.js
+++ src/components/projects/GoogleAuth.js
@@ -5,7 +5,6 @@ class GoogleAuth extends Component {
     window.gapi.load('auth2', () => {
       window.gapi.auth2.init({
         client_id: '531838176669-tpcq9fg22j2m3sgh0d95kcesffgaoo7j.apps.googleusercontent.com',
-        // client_id: '1058805176091-t1mktd0pj9hr07a2olhq7k71qr1ifalj.apps.googleusercontent.com',
         scope: 'email'
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance()
